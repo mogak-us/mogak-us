0c08248cb4fb704b6f4d51c69eaa240beb424886
malkoG <rijgndqw012@gmail.com>
2025-02-08 10:34:06 +0900

    Add development settings for Docker and ASGI configuration

--- mogak_us/asgi.py
+++ mogak_us/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mogak_us.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mogak_us.settings.development")
 
 application = get_asgi_application()
