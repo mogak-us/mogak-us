4bb8eea2237abbee3a7d68e5d258b6ac4f09c381
malkoG <rijgndqw012@gmail.com>
2025-02-09 14:15:46 +0900

    Add WorkspaceLayout and WorkspaceSidebar components for workspace's common UI

---
+++ static/src/js/layouts/WorkspaceLayout.vue
@@ -0,0 +1,21 @@
+<script setup>
+import WorkspaceSidebar from '@/layouts/WorkspaceSidebar.vue'
+
+defineProps({
+  workspace: {
+    type: Object,
+    required: true,
+    validator: (obj) => 'id' in obj && 'name' in obj,
+  },
+})
+
+</script>
+
+<template>
+  <div class="w-full flex">
+    <WorkspaceSidebar :workspace="workspace" />
+    <div class="w-full mx-8 mt-12">
+      <slot />
+    </div>
+  </div>
+</template>
---
+++ static/src/js/layouts/WorkspaceSidebar.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  workspace: {
+    type: Object,
+    required: true,
+    validator: (obj) => 'id' in obj && 'name' in obj,
+  },
+});
+</script>
+<template>
+  <div class="w-60 bg-amber-300 border-r-amber-800 border-r-2 h-screen">
+    <div class="p-4">
+      <h3>{{ workspace.name }}</h3>
+      <p>ID: {{ workspace.id }}</p>
+    </div>
+  </div>
+</template>
